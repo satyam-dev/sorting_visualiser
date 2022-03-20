a0c90e037febc04d9f3d8880033ee3b526fd2d42
Satyam Saluja <satyam.dev04@gmail.com>
2022-03-21 02:28:20 +0530

    some change
    some change

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Visualiser from "./components/visualiser";
 function App() {
   return (
     <React.Fragment>
-      <a href="upi://pay?pa=satyam.saluja@ybl&pn=Satyam&tn=Message&cu=INR&am=1">PAY NOW</a>
       <Visualiser />
     </React.Fragment>
   );
